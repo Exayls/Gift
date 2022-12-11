c8a57b6746b97f249ee5659682172e9a9a080a29
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2022-12-11 20:22:10 +0100

    add test for label at position 10

--- TestGift/UI/GiftPromptTest.cs
+++ TestGift/UI/GiftPromptTest.cs
@@ -40,5 +40,21 @@ namespace TestGift.UI
                 Assert.Equal("                              Hello", output.ToString());
             }
         }
+        [Fact]
+        public void CanDisplayTextToPosFirstLineAt10()
+        {
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var ui = new GiftUI(new Renderer(writer));
+                var position = new Position(0, 10);
+                var element = new Label("test", position);
+                ui.setChild(element);
+
+                ui.Render();
+
+                Assert.Equal("          Hello", output.ToString());
+            }
+        }
     }
 }
