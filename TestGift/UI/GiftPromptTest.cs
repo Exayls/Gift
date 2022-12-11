7d854fa55de27e269a4b72f76c903c4a1fe470ee
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2022-12-11 20:21:18 +0100

    add test for label in first line at postion 30

--- TestGift/UI/GiftPromptTest.cs
+++ TestGift/UI/GiftPromptTest.cs
@@ -24,5 +24,21 @@ namespace TestGift.UI
                 Assert.Equal("Hello", output.ToString());
             }
         }
+        [Fact]
+        public void CanDisplayTextToPosFirstLineAt30()
+        {
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var ui = new GiftUI(new Renderer(writer));
+                var position = new Position(0, 30);
+                var element = new Label("test", position);
+                ui.setChild(element);
+
+                ui.Render();
+
+                Assert.Equal("                              Hello", output.ToString());
+            }
+        }
     }
 }
