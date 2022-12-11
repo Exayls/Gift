396e831c8e7daaf823630eb44579baf0f234d227
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2022-12-11 19:04:26 +0100

    add test for label at base position

---
+++ TestGift/UI/GiftPromptTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Gift;
+using Gift.UI;
+using Gift.UI.MetaData;
+using System.Text;
+
+namespace TestGift.UI
+{
+    public class GiftPromptTest
+    {
+        [Fact]
+        public void CanDisplayTextToPosNeutral()
+        {
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var ui = new GiftUI(new Renderer(writer));
+                var position = new Position(0, 0);
+                var element = new Label("Hello", position);
+                ui.setChild(element);
+
+                ui.Render();
+
+                Assert.Equal("Hello", output.ToString());
+            }
+        }
+    }
+}
