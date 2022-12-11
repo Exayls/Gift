50956c0105c9d7a8dc33c66ce68f153cb16d6f29
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2022-12-11 21:25:55 +0100

    update exemple label

--- ExempleGift/Program.cs
+++ ExempleGift/Program.cs
@@ -2,8 +2,10 @@
 using ExempleGift;
 using Gift;
 using Gift.UI;
+using Gift.UI.MetaData;
 using System.Text;
 
 
 var ui = new GiftUI(new Renderer(Console.Out));
+ui.setChild(new Label("coucou", new Position(0, 900)));
 ui.Render();
