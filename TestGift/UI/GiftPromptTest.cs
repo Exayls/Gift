61702064db99bbb9d2e5cce47bc6bf9dc305fe6a
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2022-12-11 21:01:03 +0100

    add test label with an other string

--- TestGift/UI/GiftPromptTest.cs
+++ TestGift/UI/GiftPromptTest.cs
@@ -32,7 +32,7 @@ namespace TestGift.UI
             {
                 var ui = new GiftUI(new Renderer(writer));
                 var position = new Position(0, 30);
-                var element = new Label("test", position);
+                var element = new Label("Hello", position);
                 ui.setChild(element);
 
                 ui.Render();
@@ -48,12 +48,28 @@ namespace TestGift.UI
             {
                 var ui = new GiftUI(new Renderer(writer));
                 var position = new Position(0, 10);
-                var element = new Label("test", position);
+                var element = new Label("Hello", position);
                 ui.setChild(element);
 
                 ui.Render();
 
                 Assert.Equal("          Hello", output.ToString());
+            }
+        }
+        [Fact]
+        public void CanDisplayText2ToPosFirstLineAt10()
+        {
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var ui = new GiftUI(new Renderer(writer));
+                var position = new Position(0, 10);
+                var element = new Label("test", position);
+                ui.setChild(element);
+
+                ui.Render();
+
+                Assert.Equal("          test", output.ToString());
             }
         }
     }
