0a025c0dd5863e19fc2bcfc35a1a8931d1610504
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-10 21:42:55 +0100

    rename position variable

--- Gift/UI/VStack.cs
+++ Gift/UI/VStack.cs
@@ -12,13 +12,13 @@ namespace Gift.UI
         public void AddChild(UIElement uIElement)
         {
             Childs.Add(uIElement);
-            int y = 0;
+            int GlobalPositionLastChild = 0;
             if (Childs.Count != 0)
             {
-                y = Context?.GlobalPosition?.y?? 0;
+                GlobalPositionLastChild = Context?.GlobalPosition?.y?? 0;
             }
             uIElement.Renderer = Renderer;
-            uIElement.setContext(new Position(y, Context?.GlobalPosition?.x?? 0), new Bound(0, Context?.Bounds?.Width ?? 0));
+            uIElement.setContext(new Position(GlobalPositionLastChild, Context?.GlobalPosition?.x?? 0), new Bound(0, Context?.Bounds?.Width ?? 0));
         }
     }
 }
