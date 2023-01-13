dcd9932912acccd63a633a2f98c889d65500b875
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-13 22:55:25 +0100

    fix method name

--- ExempleGift/Program.cs
+++ ExempleGift/Program.cs
@@ -7,5 +7,5 @@ using System.Text;
 
 
 var ui = new GiftUI(new Renderer(Console.Out));
-ui.setChild(new Label("coucou", new Position(0, 58)));
+ui.SetChild(new Label("coucou", new Position(0, 58)));
 ui.Render();
