c682c6bfb8d25d3a03662991d55ac094319a6a61
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-14 01:07:12 +0100

    doc label builder

--- Gift/Builders/LabelBuilder.cs
+++ Gift/Builders/LabelBuilder.cs
@@ -16,15 +16,28 @@ namespace Gift.Builders
         private string text = "Hello";
         private Position position = new Position(0,0);
 
+        /// <summary>
+        /// Get Label instance with "Hello" as default text and (0,0) as default position
+        /// </summary>
         public LabelBuilder()
         {
 
         }
+        /// <summary>
+        /// Set text parameter to the builder
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>LabelBuilder instance</returns>
         public LabelBuilder WithText(string text)
         {
             this.text = text;
             return this;
         }
+        /// <summary>
+        /// Set position parameter to the builder
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns>LabelBuilder instance</returns>
         public LabelBuilder WithPosition(Position position)
         {
             this.position = position;
