f19b830ce8803f70aa05c990d54e1e5b5b52d444
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-17 22:08:44 +0100

    reformat

--- TestGift/Builder/GiftUIBuilderTest.cs
+++ TestGift/Builder/GiftUIBuilderTest.cs
@@ -18,18 +18,19 @@ namespace TestGift.Builder
             GiftUIBuilder builder = new GiftUIBuilder();
             GiftUI g = builder.Build();
 
-            Assert.Equal(20,g.Bound.Height);
-            Assert.Equal(60,g.Bound.Width);
+            Assert.Equal(20, g.Bound.Height);
+            Assert.Equal(60, g.Bound.Width);
         }
         [Fact]
         public void BuilderPositionTest()
         {
 
-            GiftUIBuilder builder = new GiftUIBuilder();
-            GiftUI g = builder.WithBound(new Bound(1,23)).Build();
 
-            Assert.Equal(1,g.Bound.Height);
-            Assert.Equal(23,g.Bound.Width);
+            GiftUIBuilder builder = new GiftUIBuilder();
+            GiftUI g = builder.WithBound(new Bound(1, 23)).Build();
+
+            Assert.Equal(1, g.Bound.Height);
+            Assert.Equal(23, g.Bound.Width);
         }
     }
 }
