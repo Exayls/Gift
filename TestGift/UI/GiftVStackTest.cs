f4e01d186f4c13239c6f1f69ef3f2515f6524867
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-28 23:17:35 +0100

    test vstack out of bound

--- TestGift/UI/GiftVStackTest.cs
+++ TestGift/UI/GiftVStackTest.cs
@@ -255,9 +255,56 @@ namespace TestGift.UI
                     {
                         expected = TestHelper.Replace(expected, "label numero 4.", 0);
                     }
-                    if (i == 4)
+                    Assert.Equal(expected, actual[i]);
+                }
+            }
+        }
+        [Fact]
+        public void TestVStackWithTooMuchElement2()
+        {
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var ui = new GiftUI(new Bound(4, 60));
+                var vstack = new VStackBuilder().Build();
+                var label1 = new LabelBuilder().BuildImplicit();
+                var label2 = new LabelBuilder().WithText("test").BuildImplicit();
+                var label3 = new LabelBuilder().WithText("label numero 3.").BuildImplicit();
+                var label4 = new LabelBuilder().WithText("label numero 4.").BuildImplicit();
+                var label5 = new LabelBuilder().WithText("label numero 5.").BuildImplicit();
+                var label6 = new LabelBuilder().WithText("label numero 6.").BuildImplicit();
+                ui.SetChild(vstack);
+                vstack.AddChild(label1);
+                vstack.AddChild(label2);
+                vstack.AddChild(label3);
+                vstack.AddChild(label4);
+                vstack.AddChild(label5);
+                vstack.AddChild(label6);
+                TextWriter renderedText = new Renderer().GetRenderedBuffer(ui);
+
+                var expectedBuilder = new StringBuilder();
+                string expected = "";
+                var actual = renderedText.ToString().Split('\n');
+                for (int i = 0; i < ui.Bound.Height; i++)
+                {
+                    expectedBuilder.Clear();
+                    expectedBuilder.Append(new string(GiftBase.FILLINGCHAR, ui.Bound.Width));
+                    expected = expectedBuilder.ToString();
+                    if (i == 0)
                     {
-                        expected = TestHelper.Replace(expected, "label numero 5.", 0);
+                        expected = TestHelper.Replace(expected, "Hello", 0);
+                    }
+                    if (i == 1)
+                    {
+                        expected = TestHelper.Replace(expected, "test", 0);
+                    }
+                    if (i == 2)
+                    {
+                        expected = TestHelper.Replace(expected, "label numero 3.", 0);
+                    }
+                    if (i == 3)
+                    {
+                        expected = TestHelper.Replace(expected, "label numero 4.", 0);
                     }
                     Assert.Equal(expected, actual[i]);
                 }
