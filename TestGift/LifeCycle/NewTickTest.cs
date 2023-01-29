3bdd2ae164416af15630373073e65dcfe56a8b73
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-01-29 18:25:14 +0100

    add test generation tick

---
+++ TestGift/LifeCycle/NewTickTest.cs
@@ -0,0 +1,27 @@
+﻿using Gift;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGift.LifeCycle
+{
+    public class NewTickTest
+    {
+        [Fact]
+        public void TestTickExist()
+        {
+            var GiftBase = new GiftBase();
+            GiftBase.Tick();
+        }
+        [Fact]
+        public void TestTickReturnNewRender()
+        {
+            var GiftBase = new GiftBase();
+            GiftBase.GetCurrentView();
+            GiftBase.Tick();
+
+        }
+    }
+}
