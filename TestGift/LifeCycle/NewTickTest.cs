6a97fc5af55e87df2df32d7fe31404d0d1a45927
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-02-04 15:31:17 +0100

    add mock

--- TestGift/LifeCycle/NewTickTest.cs
+++ TestGift/LifeCycle/NewTickTest.cs
@@ -4,21 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace TestGift.LifeCycle
 {
     public class NewTickTest
     {
+        private Mock<Renderer> _rendererMock;
+
+        public NewTickTest()
+        {
+            _rendererMock = new Mock<Renderer>();
+        }
         [Fact]
         public void TestTickExist()
         {
-            var GiftBase = new GiftBase();
+            var GiftBase = new GiftBase(_rendererMock.Object);
             GiftBase.Tick();
         }
         [Fact]
         public void TestTickReturnNewRender()
         {
-            var GiftBase = new GiftBase();
+            var GiftBase = new GiftBase(_rendererMock.Object);
             GiftBase.GetCurrentView();
             GiftBase.Tick();
 
