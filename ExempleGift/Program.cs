21280083442ce80a34dd135017608bdb3e15e74e
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-02-13 13:50:58 +0100

    change render method in example

--- ExempleGift/Program.cs
+++ ExempleGift/Program.cs
@@ -8,4 +8,4 @@ using System.Text;
 
 var ui = new GiftUI();
 ui.SetChild(new Label("coucou", new Position(2, 58)));
-Console.Out.Write(new Renderer().Render(ui));
+Console.Out.Write(new Renderer().GetRenderedBuffer(ui));
