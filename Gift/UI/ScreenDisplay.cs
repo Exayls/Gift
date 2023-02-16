ce85e8c701408343e9a0c6e44964120266fa059c
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-02-16 22:36:13 +0100

    pass test

--- Gift/UI/ScreenDisplay.cs
+++ Gift/UI/ScreenDisplay.cs
@@ -8,15 +8,53 @@ namespace Gift.UI
         public Bound TotalBound { get; }
         public StringBuilder DisplayString { get; }
 
-        public ScreenDisplay()
-        {
-            DisplayString = new StringBuilder();
-        }
 
-        public ScreenDisplay(Bound bound)
+        public ScreenDisplay(Bound bound) : this(bound, GiftBase.FILLINGCHAR)
+        {
+        }
+        public ScreenDisplay(Bound bound, char emptychar)
         {
             DisplayString = new StringBuilder();
             TotalBound = bound;
+            FillDisplayString(emptychar);
+        }
+
+        private void FillDisplayString(char emptychar)
+        {
+            DisplayString.Append(new string(emptychar, TotalBound.Width));
+            for (int i = 1; i < TotalBound.Height; i++)
+            {
+                DisplayString.Append('\n');
+                DisplayString.Append(new string(emptychar, TotalBound.Width));
+            }
+        }
+
+        public void AddDisplay(ScreenDisplay display, Position globalPosition)
+        {
+            for (int i = 0; i < display.TotalBound.Height; i++)
+            {
+                if (globalPosition.x == -2)
+                {
+                    int indexLineToReplace = (globalPosition.y + i) * (TotalBound.Width + 1) + globalPosition.x+2;
+                    int lenghtToReplace = display.TotalBound.Width - 2;
+                    DisplayString.Remove(indexLineToReplace, lenghtToReplace).Insert(indexLineToReplace, display.GetLine(i).Substring(0, lenghtToReplace));
+                }
+                else
+                {
+                    int indexLineToReplace = (globalPosition.y + i) * (TotalBound.Width + 1) + globalPosition.x;
+                    DisplayString.Remove(indexLineToReplace, display.TotalBound.Width).Insert(indexLineToReplace, display.GetLine(i));
+                }
+            }
+        }
+
+        private string GetLine(int i)
+        {
+            return DisplayString.ToString().Substring((i * (TotalBound.Width + 1)), TotalBound.Width - i);
+        }
+
+        internal void AddString(string display, Position position)
+        {
+            Helper.Replace(DisplayString, display, position.y * (TotalBound.Width + 1) + position.x);
         }
     }
 }
