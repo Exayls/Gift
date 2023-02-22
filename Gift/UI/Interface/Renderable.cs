84a2e0a37c84f997464990d1d8869a70ee995e97
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-02-22 12:31:01 +0100

    add more interfaces

--- Gift/UI/Interface/Renderable.cs
+++ Gift/UI/Interface/Renderable.cs
@@ -1,8 +1,14 @@
-﻿using System.Text;
+﻿using Gift.UI.MetaData;
+using System.Text;
 
 namespace Gift.UI.Interface
 {
     public interface Renderable
     {
+        int Height { get; }
+
+        IScreenDisplay GetDisplay(Bound bound);
+        Position GetGlobalPosition(Context context);
+        bool IsFixed();
     }
 }
