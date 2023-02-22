40420c1e8f4db8f9d1e88a72e92e56375bae6526
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-02-22 20:05:21 +0100

    refacto vstack

--- Gift/UI/VStack.cs
+++ Gift/UI/VStack.cs
@@ -27,23 +27,9 @@ namespace Gift.UI
 
         public void AddChild(UIElement uIElement)
         {
-            int ChildContextPosition = GetHeightAllChilds();
             Childs.Add(uIElement);
-            dynamic element = uIElement;
-            setChildContext(element, ChildContextPosition);
         }
 
-        private int GetHeightAllChilds()
-        {
-            int GlobalPosition = Context?.GlobalPosition?.y ?? 0;
-            int ChildContextPosition = GlobalPosition;
-            foreach (UIElement renderable in Childs)
-            {
-                ChildContextPosition += renderable.Context?.Bounds?.Height ?? 0;
-            }
-
-            return ChildContextPosition;
-        }
 
         private int GetHeightAllChildsAfter(Renderable uIElement)
         {
@@ -70,26 +56,6 @@ namespace Gift.UI
             return true;
         }
 
-
-        private void setChildContext(Label UiElement, int ChildContextPosition)
-        {
-            if (Context == null)
-            {
-                throw new ArgumentNullException();
-            }
-            if (UiElement.IsFixed())
-            {
-                if (Context.GlobalPosition == null)
-                {
-                    throw new ArgumentNullException();
-                }
-                UiElement.setContext(Context.GlobalPosition, new Bound(0, Context?.Bounds?.Width ?? 0));
-            }
-            else
-            {
-                UiElement.setContext(new Position(ChildContextPosition, Context?.GlobalPosition?.x ?? 0), new Bound(1, Context?.Bounds?.Width ?? 0));
-            }
-        }
 
         public override Context GetContext(Renderable renderable, Context context)
         {
