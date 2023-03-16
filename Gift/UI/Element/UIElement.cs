58b3a60a9d61bf5d08070e260acd39c0fb9c8e38
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-03-16 23:45:38 +0100

    fix formatting

--- Gift/UI/Element/UIElement.cs
+++ Gift/UI/Element/UIElement.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Gift.UI.Element
 {
-    public abstract class UIElement : IRenderable, IUIElement
+    public abstract class UIElement : IUIElement
     {
 
         public abstract int Height { get; }
@@ -24,7 +24,7 @@ namespace Gift.UI.Element
 
         public abstract IScreenDisplay GetDisplay(Bound bound);
         public abstract IScreenDisplay GetDisplayWithoutBorder(Bound bound);
-        public abstract IScreenDisplay GetDisplayBorder(Bound bounds);
+        public abstract IScreenDisplay GetDisplayBorder(Bound bound);
         public abstract Position GetRelativePosition(Context context);
         public abstract Position GetGlobalPosition(Context context);
         public abstract bool IsFixed();
