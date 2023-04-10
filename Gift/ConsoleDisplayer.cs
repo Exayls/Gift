73240d702d401d7c08f220e3aa11f53fd16ff060
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-04-10 19:49:45 +0200

    write the Console display in one go

--- Gift/ConsoleDisplayer.cs
+++ Gift/ConsoleDisplayer.cs
@@ -11,16 +11,17 @@ namespace Gift
 
         public void display(IScreenDisplay screenDisplay)
         {
-            StringBuilder displayString = screenDisplay.DisplayString;
+            string displayString = "";
             char[,] displayMap = screenDisplay.DisplayMap;
-            Console.SetCursorPosition(0, 0);
             for (int i = 0; i < displayMap.GetLength(0); i++)
             {
                 for (int j = 0; j < displayMap.GetLength(1); j++)
                 {
-                    Console.Out.Write(displayMap[i,j]);
+                    displayString += displayMap[i, j];
                 }
             }
+            Console.SetCursorPosition(0, 0);
+            Console.Out.Write(displayString);
         }
     }
 }
