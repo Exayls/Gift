1c9885cf4de203bc57ad94d66f221d2af801c1ff
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-04-18 22:54:38 +0200

    add console size monitor

---
+++ Gift/Event/ConsoleSizeMonitor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class ConsoleSizeMonitor
+{
+    public event EventHandler? SizeChanged;
+
+    private int ConsoleWidth;
+    private int ConsoleHeight;
+
+    public ConsoleSizeMonitor()
+    {
+        ConsoleWidth = Console.WindowWidth;
+        ConsoleHeight = Console.WindowHeight;
+
+        var timer = new Timer(CheckWindowSize, null, 0, 100);
+    }
+
+    private void CheckWindowSize(object? state)
+    {
+        if (Console.WindowWidth != ConsoleWidth || Console.WindowHeight != ConsoleHeight)
+        {
+            ConsoleWidth = Console.WindowWidth;
+            ConsoleHeight = Console.WindowHeight;
+
+            OnSizeChanged();
+        }
+    }
+
+    protected virtual void OnSizeChanged()
+    {
+        SizeChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
