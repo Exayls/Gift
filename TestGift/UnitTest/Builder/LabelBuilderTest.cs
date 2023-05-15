c0ff4e9235bb1f7293e0a7c0fc84f9fe146be5d8
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-05-15 19:27:40 +0200

    test label color

--- TestGift/UnitTest/Builder/LabelBuilderTest.cs
+++ TestGift/UnitTest/Builder/LabelBuilderTest.cs
@@ -1,5 +1,6 @@
 ﻿using Gift.Builders;
 using Gift.UI.Element;
+using Gift.UI.MetaData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,9 @@ namespace TestGift.Builder
             LabelBuilder builder = new LabelBuilder();
             Label l = builder.Build();
 
-            Assert.Equal("Hello",l.Text );
+            Assert.Equal("Hello", l.Text);
         }
+
         [Fact]
         public void BuilderPositionTest()
         {
@@ -26,8 +28,21 @@ namespace TestGift.Builder
             LabelBuilder builder = new LabelBuilder();
             Label l = builder.Build();
 
-            Assert.Equal(0,l.Disposition.Position.y );
-            Assert.Equal(0,l.Disposition.Position.x );
+            Assert.Equal(0, l.Disposition.Position.y);
+            Assert.Equal(0, l.Disposition.Position.x);
+        }
+
+        [Fact]
+        public void BuilderBackgroundTest()
+        {
+
+            LabelBuilder builder = new LabelBuilder()
+                .WithBackgroundColor(Color.Green)
+                .WithForegroundColor(Color.Red);
+            Label l = builder.Build();
+
+            Assert.Equal(Color.Green, l.BackColor);
+            Assert.Equal(Color.Red, l.FrontColor);
         }
     }
 }
