958cc953e71cb75dedc00f925c989343b8bb1b9f
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-05-22 23:21:54 +0200

    add giftuiprovider

---
+++ Gift/src/UIModel/GiftUiProvider.cs
@@ -0,0 +1,21 @@
+﻿using Gift.UI;
+
+namespace Gift.src.UIModel
+{
+    public class GiftUiProvider : IGiftUiProvider
+    {
+        private IGiftUI? _ui;
+        public IGiftUI Ui
+        {
+            get
+            {
+                return _ui ?? new GiftUI();
+            }
+            set
+            {
+                _ui = value;
+            }
+        }
+
+    }
+}
