283f7bd5b06dbdd483e8d07c3d17abc98c77899c
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-06-09 23:57:16 +0200

    add test scroll up

--- TestGift/UnitTest/UI/Element/VstackTest.cs
+++ TestGift/UnitTest/UI/Element/VstackTest.cs
@@ -386,6 +386,24 @@ namespace TestGift.UnitTest.UI.Element
             Assert.Equal(0, context.Position.x);
         }
 
+        [Fact]
+        public void GetContext_should_return_context_with_1_0_position_when_vstack_has_scrollUp()
+        {
+            //arrange
+            _borderMock.Setup(b => b.Thickness).Returns(2);
+            _uiElementMock1.Setup(u => u.IsFixed()).Returns(false);
+            _uiElementMock1.Setup(u => u.Height).Returns(3);
+            _uiElementMock1.Setup(u => u.Width).Returns(1);
+            vstack.AddChild(_uiElementMock1.Object);
+            vstack.ScrollUp();
+            Context contextRenderable = new Context(new Position(0, 0), new Bound(5, 5));
+            //act
+            Context context = vstack.GetContextRelativeRenderable(_uiElementMock1.Object, contextRenderable);
+            //assert
+            Assert.Equal(1, context.Position.y);
+            Assert.Equal(0, context.Position.x);
+        }
+
         //Height/Width Tests
         [Fact]
         public void Height_should_be_1_when_1_element_height_1_in_it_with_no_border()
