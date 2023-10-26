461e4a22945ef2454b4f9616e21432a7c2d31188
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2023-10-26 23:33:50 +0200

    correct generic types in interface

--- src/Gift.Domain/ServiceContracts/IUIElementRegister.cs
+++ src/Gift.Domain/ServiceContracts/IUIElementRegister.cs
@@ -6,8 +6,8 @@ namespace Gift.Domain.ServiceContracts
     {
         Type GetTypeByName(string typeName);
         void Register(string name, Type type);
-        void Register<T>(string elementName, string attributeName, Func<T,T> builderMethod);
+        void Register<T>(string elementName, string attributeName, Func<T, string, T> builderMethod);
         void Register<T>(string v);
-        void Register<T1, T2>(string elementName, string attributeName, Func<T1, T1> BuilderMethod);
+        void Register<T1, T2>(string elementName, string attributeName, Func<T1, T2, T1> BuilderMethod);
     }
 }
