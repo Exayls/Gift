05b2a65b52c68335d8eb20734acf53c542771942
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2024-01-05 20:42:00 +0100

    format

--- src/Gift.Domain/Builders/VStackBuilder.cs
+++ src/Gift.Domain/Builders/VStackBuilder.cs
@@ -8,166 +8,166 @@ using Gift.Domain.UIModel.MetaData;
 
 namespace Gift.Domain.Builders
 {
-    public class VStackBuilder : IContainerBuilder
-    {
-        private IBorder _border = new NoBorder();
-        private Bound _bound = new Bound(0, 0);
-        private IScreenDisplayFactory screenDisplayFactory = new ScreenDisplayFactory();
-        private Color backColor = Color.Default;
-        private Color frontColor = Color.Default;
-        private IList<UIElement> selectableElements = new List<UIElement>();
-        private IList<UIElement> unSelectableElements = new List<UIElement>();
-        private int? _height;
-        private int? _width;
+	public class VStackBuilder : IContainerBuilder
+	{
+		private IBorder _border = new NoBorder();
+		private Bound _bound = new Bound(0, 0);
+		private IScreenDisplayFactory screenDisplayFactory = new ScreenDisplayFactory();
+		private Color backColor = Color.Default;
+		private Color frontColor = Color.Default;
+		private IList<UIElement> selectableElements = new List<UIElement>();
+		private IList<UIElement> unSelectableElements = new List<UIElement>();
+		private int? _height;
+		private int? _width;
 
-        public VStackBuilder WithBorder(IBorder border)
-        {
-            _border = border;
-            return this;
-        }
-        public VStackBuilder WithBound(Bound bound)
-        {
-            _bound = bound;
-            return this;
-        }
+		public VStackBuilder WithBorder(IBorder border)
+		{
+			_border = border;
+			return this;
+		}
+		public VStackBuilder WithBound(Bound bound)
+		{
+			_bound = bound;
+			return this;
+		}
 
-        public VStackBuilder WithBackgroundColor(Color color)
-        {
-            backColor = color;
-            return this;
-        }
+		public VStackBuilder WithBackgroundColor(Color color)
+		{
+			backColor = color;
+			return this;
+		}
 
-        public VStackBuilder WithForegroundColor(Color color)
-        {
-            frontColor = color;
-            return this;
-        }
+		public VStackBuilder WithForegroundColor(Color color)
+		{
+			frontColor = color;
+			return this;
+		}
 
-        public VStackBuilder WithSelectableElement(UIElement element)
-        {
-            selectableElements.Add(element);
-            return this;
-        }
+		public VStackBuilder WithSelectableElement(UIElement element)
+		{
+			selectableElements.Add(element);
+			return this;
+		}
 
-        public VStackBuilder WithUnSelectableElement(UIElement element)
-        {
-            unSelectableElements.Add(element);
-            return this;
-        }
-
-
-        public VStackBuilder WithHeight(int height)
-        {
-            _height = height;
-            return this;
-        }
-
-        public VStackBuilder WithWidth(int width)
-        {
-            _width = width;
-            return this;
-        }
+		public VStackBuilder WithUnSelectableElement(UIElement element)
+		{
+			unSelectableElements.Add(element);
+			return this;
+		}
 
 
-        public VStack Build()
-        {
-			var bound = new Bound(_height??_bound.Height, _width??_bound.Width);
-            var vstack =  new VStack(_border,
-                              screenDisplayFactory,
-                              bound,
-                              frontColor: frontColor,
-                              backColor: backColor);
-			foreach(UIElement element in unSelectableElements)
+		public VStackBuilder WithHeight(int height)
+		{
+			_height = height;
+			return this;
+		}
+
+		public VStackBuilder WithWidth(int width)
+		{
+			_width = width;
+			return this;
+		}
+
+
+		public VStack Build()
+		{
+			var bound = new Bound(_height ?? _bound.Height, _width ?? _bound.Width);
+			var vstack = new VStack(_border,
+							  screenDisplayFactory,
+							  bound,
+							  frontColor: frontColor,
+							  backColor: backColor);
+			foreach (UIElement element in unSelectableElements)
 			{
 				vstack.AddUnselectableChild(element);
 			}
-			foreach(UIElement element in selectableElements)
+			foreach (UIElement element in selectableElements)
 			{
 				vstack.AddSelectableChild(element);
 			}
 			return vstack;
-        }
+		}
 
-        UIElement IBuilder<UIElement>.Build()
-        {
-            return Build();
-        }
+		UIElement IBuilder<UIElement>.Build()
+		{
+			return Build();
+		}
 
-        Container IBuilder<Container>.Build()
-        {
-            return Build();
-        }
+		Container IBuilder<Container>.Build()
+		{
+			return Build();
+		}
 
-        IContainerBuilder IContainerBuilder.WithBound(Bound bound)
-        {
-            return WithBound(bound);
-        }
+		IContainerBuilder IContainerBuilder.WithBound(Bound bound)
+		{
+			return WithBound(bound);
+		}
 
-        IContainerBuilder IContainerBuilder.WithSelectableElement(UIElement element)
-        {
-            return WithSelectableElement(element);
-        }
+		IContainerBuilder IContainerBuilder.WithSelectableElement(UIElement element)
+		{
+			return WithSelectableElement(element);
+		}
 
-        IContainerBuilder IContainerBuilder.WithUnSelectableElement(UIElement element)
-        {
+		IContainerBuilder IContainerBuilder.WithUnSelectableElement(UIElement element)
+		{
 			return WithUnSelectableElement(element);
-        }
+		}
 
-        IUIElementBuilder IUIElementBuilder.WithBorder(IBorder border)
-        {
-            return WithBorder(border);
-        }
+		IUIElementBuilder IUIElementBuilder.WithBorder(IBorder border)
+		{
+			return WithBorder(border);
+		}
 
-        IUIElementBuilder IUIElementBuilder.WithBackgroundColor(Color color)
-        {
-            return WithBackgroundColor(color);
-        }
+		IUIElementBuilder IUIElementBuilder.WithBackgroundColor(Color color)
+		{
+			return WithBackgroundColor(color);
+		}
 
-        IUIElementBuilder IUIElementBuilder.WithForegroundColor(Color color)
-        {
-            return WithForegroundColor(color);
-        }
+		IUIElementBuilder IUIElementBuilder.WithForegroundColor(Color color)
+		{
+			return WithForegroundColor(color);
+		}
 
-        IContainerBuilder IContainerBuilder.WithHeight(int height)
-        {
-            return WithHeight(height);
-        }
+		IContainerBuilder IContainerBuilder.WithHeight(int height)
+		{
+			return WithHeight(height);
+		}
 
-        IContainerBuilder IContainerBuilder.WithWidth(int width)
-        {
-            return WithWidth(width);
-        }
+		IContainerBuilder IContainerBuilder.WithWidth(int width)
+		{
+			return WithWidth(width);
+		}
 
 
-        public IUIElementBuilder WithBorder(string borderStr, IBorderMapper mapper)
-        {
+		public IUIElementBuilder WithBorder(string borderStr, IBorderMapper mapper)
+		{
 			return WithBorder(mapper.ToBorder(borderStr));
-        }
+		}
 
-        public IUIElementBuilder WithBackgroundColor(string colorStr, IColorMapper mapper)
-        {
+		public IUIElementBuilder WithBackgroundColor(string colorStr, IColorMapper mapper)
+		{
 			return WithBackgroundColor(mapper.ToColor(colorStr));
-        }
+		}
 
-        public IUIElementBuilder WithForegroundColor(string colorStr, IColorMapper mapper)
-        {
+		public IUIElementBuilder WithForegroundColor(string colorStr, IColorMapper mapper)
+		{
 			return WithForegroundColor(mapper.ToColor(colorStr));
-        }
+		}
 
-        public IContainerBuilder WithBound(string boundStr, IBoundMapper mapper)
-        {
-            return WithBound(mapper.ToBound(boundStr));
-        }
+		public IContainerBuilder WithBound(string boundStr, IBoundMapper mapper)
+		{
+			return WithBound(mapper.ToBound(boundStr));
+		}
 
-        public IContainerBuilder WithHeight(string heightStr)
-        {
-            return WithHeight(int.Parse(heightStr, NumberStyles.Integer));
-        }
+		public IContainerBuilder WithHeight(string heightStr)
+		{
+			return WithHeight(int.Parse(heightStr, NumberStyles.Integer));
+		}
 
-        public IContainerBuilder WithWidth(string widthStr)
-        {
-            return WithWidth(int.Parse(widthStr, NumberStyles.Integer));
-        }
+		public IContainerBuilder WithWidth(string widthStr)
+		{
+			return WithWidth(int.Parse(widthStr, NumberStyles.Integer));
+		}
 
-    }
+	}
 }
