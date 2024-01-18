c7265abcc36e2dd8d6e5d3068359e7e056070c8c
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2024-01-18 20:39:08 +0100

    test colormap rendering

--- test/Gift.Displayer.Tests/Integration/RendererTest.cs
+++ test/Gift.Displayer.Tests/Integration/RendererTest.cs
@@ -6,23 +6,36 @@ using Gift.Domain.UIModel.Conf;
 using Gift.Domain.UIModel.Display;
 using Gift.Domain.UIModel.Element;
 using Gift.Domain.UIModel.MetaData;
+using Gift.XmlUiParser.Tests.Helper;
+using Microsoft.Extensions.Logging;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace TestGift.Test.UI
 {
     public class RendererTest
     {
         private Renderer renderer;
+        private readonly ITestOutputHelper _output;
 
-        public RendererTest() { renderer = new Renderer(new DefaultConfiguration()); }
+        public RendererTest(ITestOutputHelper output)
+        {
+            renderer = new Renderer(new DefaultConfiguration());
+            _output = output;
+        }
 
         [Fact]
         public void Can_render_Simple_UI()
         {
             GiftUI ui = CreateGiftUi(new Bound(5, 10), new NoBorder());
             IScreenDisplay rendered = renderer.GetRenderDisplay(ui);
-            const string expected = "**********\n" + "**********\n" + "**********\n" +
-                                    "**********\n" + "**********";
+            // clang-format off
+			const string expected = "**********\n" +
+									"**********\n" +
+									"**********\n" +
+									"**********\n" +
+									"**********";
+            // clang-format on
             Assert.Equal(expected, rendered.DisplayString.ToString());
         }
 
@@ -36,34 +49,32 @@ namespace TestGift.Test.UI
         {
             GiftUI ui = CreateGiftUi(new Bound(10, 10), new NoBorder());
 
-            VStack vstack =
-                new VStackBuilder()
-                    .WithBorder(new DetailedBorder(
-                        1, BorderOption.GetBorderCharsFromFile(
-                               "ressources/borderchars/double_border.json")))
-                    .Build();
+            VStack vstack = new VStackBuilder()
+                                .WithBorder(new DetailedBorder(1, BorderOption.GetBorderCharsFromFile(
+                                                                      "ressources/borderchars/double_border.json")))
+                                .Build();
             vstack.AddUnselectableChild(new LabelBuilder().Build());
             ui.AddUnselectableChild(vstack);
-            VStack vstack2 =
-                new VStackBuilder()
-                    .WithBorder(new DetailedBorder(
-                        1, BorderOption.GetBorderCharsFromFile(
-                               "ressources/borderchars/simple_border.json")))
-                    .Build();
+            VStack vstack2 = new VStackBuilder()
+                                 .WithBorder(new DetailedBorder(1, BorderOption.GetBorderCharsFromFile(
+                                                                       "ressources/borderchars/simple_border.json")))
+                                 .Build();
             vstack.AddUnselectableChild(vstack2);
             vstack2.AddUnselectableChild(new LabelBuilder().WithText("hey").Build());
             vstack2.AddUnselectableChild(new LabelBuilder().Build());
             IScreenDisplay rendered = renderer.GetRenderDisplay(ui);
-            const string expected = "╔════════╗\n" +
-                                    "║Hello***║\n" +
-                                    "║┌─────┐*║\n" +
-                                    "║│hey**│*║\n" +
-                                    "║│Hello│*║\n" +
-                                    "║└─────┘*║\n" +
-                                    "║********║\n" +
-                                    "║********║\n" +
-                                    "║********║\n" +
-                                    "╚════════╝";
+            // clang-format off
+			const string expected = "╔════════╗\n" +
+									"║Hello***║\n" +
+									"║┌─────┐*║\n" +
+									"║│hey**│*║\n" +
+									"║│Hello│*║\n" +
+									"║└─────┘*║\n" +
+									"║********║\n" +
+									"║********║\n" +
+									"║********║\n" +
+									"╚════════╝";
+            // clang-format on
             Assert.Equal(expected, rendered.DisplayString.ToString());
         }
 
@@ -72,39 +83,84 @@ namespace TestGift.Test.UI
         {
             GiftUI ui = CreateGiftUi(new Bound(10, 10), new NoBorder());
 
-            VStack vstack =
-                new VStackBuilder()
-                    .WithBorder(new DetailedBorder(
-                        1, BorderOption.GetBorderCharsFromFile(
-                               "ressources/borderchars/double_border.json")))
-                    .Build();
+            VStack vstack = new VStackBuilder()
+                                .WithBorder(new DetailedBorder(1, BorderOption.GetBorderCharsFromFile(
+                                                                      "ressources/borderchars/double_border.json")))
+                                .Build();
             vstack.AddUnselectableChild(new LabelBuilder().Build());
             ui.AddUnselectableChild(vstack);
-            VStack vstack2 =
-                new VStackBuilder()
-                    .WithBorder(new DetailedBorder(
-                        1, BorderOption.GetBorderCharsFromFile(
-                               "ressources/borderchars/simple_border.json")))
-                    .Build();
+            VStack vstack2 = new VStackBuilder()
+                                 .WithBorder(new DetailedBorder(1, BorderOption.GetBorderCharsFromFile(
+                                                                       "ressources/borderchars/simple_border.json")))
+                                 .Build();
             vstack.AddUnselectableChild(vstack2);
             vstack2.AddUnselectableChild(new LabelBuilder().WithText("hey").Build());
-            vstack2.AddUnselectableChild(new LabelBuilder()
-                                             .WithText("test6")
-                                             .WithPosition(new Position(-2, 3))
-                                             .Build());
+            vstack2.AddUnselectableChild(
+                new LabelBuilder().WithText("test6").WithPosition(new Position(-2, 3)).Build());
             vstack2.AddUnselectableChild(new LabelBuilder().Build());
             IScreenDisplay rendered = renderer.GetRenderDisplay(ui);
-            const string expected = "╔════════╗\n" +
-                                    "║Hello***║\n" +
-                                    "║┌─────┐*║\n" +
-                                    "║│hey**│*║\n" +
-                                    "║│Hello│*║\n" +
-                                    "║└─────┘*║\n" +
-                                    "║********║\n" +
-                                    "║********║\n" +
-                                    "║********║\n" +
-                                    "╚════════╝";
+
+            // clang-format off
+			const string expected = "╔════════╗\n" +
+									"║Hello***║\n" +
+									"║┌─────┐*║\n" +
+									"║│hey**│*║\n" +
+									"║│Hello│*║\n" +
+									"║└─────┘*║\n" +
+									"║********║\n" +
+									"║********║\n" +
+									"║********║\n" +
+									"╚════════╝";
+            // clang-format on
             Assert.Equal(expected, rendered.DisplayString.ToString());
+        }
+
+        [Fact]
+        public void Can_render_color_of_border()
+        {
+            GiftUI ui = CreateGiftUi(new Bound(4, 10), new NoBorder());
+
+            VStack vstack =
+                new VStackBuilder().WithBorder(new SimpleBorder(1, '-')).WithForegroundColor(Color.Red).Build();
+            vstack.AddUnselectableChild(new LabelBuilder().Build());
+            ui.AddUnselectableChild(vstack);
+
+            IScreenDisplay rendered = renderer.GetRenderDisplay(ui);
+            // clang-format off
+			Color[,] expected =
+			{
+				{Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red },
+				{Color.Red, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.Red },
+				{Color.Red, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.White, Color.Red },
+				{Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red, Color.Red },
+			};
+
+            // clang-format on
+            var logger = LoggerHelper.GetLogger<RendererTest>(_output);
+
+            Print2DArray(expected, logger);
+            Print2DArray(rendered.FrontColorMap, logger);
+            Print2DArray(rendered.DisplayMap, logger);
+            // foreach (Color line in expected.Length)
+            // {
+            //     logger.LogTrace(line.ToString());
+            // }
+
+            Assert.Equal(expected, rendered.FrontColorMap);
+        }
+
+        public static void Print2DArray<T>(T[,] matrix, ILogger logger)
+        {
+            string line = "";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    line += matrix[i, j]?.ToString() ?? "" + " ";
+                }
+                line += "\n";
+            }
+            logger.LogTrace(line);
         }
     }
 }
