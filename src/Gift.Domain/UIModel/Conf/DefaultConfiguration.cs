95cd470c16ff252e12ad05181905c5ea77a4e5e4
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2024-03-15 23:46:10 +0100

    update default color conf

--- src/Gift.Domain/UIModel/Conf/DefaultConfiguration.cs
+++ src/Gift.Domain/UIModel/Conf/DefaultConfiguration.cs
@@ -7,11 +7,11 @@ namespace Gift.Domain.UIModel.Conf
         public Color DefaultFrontColor { get { return Color.White; } }
         public Color DefaultBackColor { get { return Color.Transparent; } }
 
-        public Color SelectedElementFrontColor { get { return Color.Default; } }
+        public Color SelectedElementFrontColor { get { return Color.White; } }
         public Color SelectedElementBackColor { get { return Color.Green; } }
 
         public Color SelectedContainerFrontColor { get { return Color.Green; } }
-        public Color SelectedContainerBackColor { get { return Color.Default; } }
+        public Color SelectedContainerBackColor { get { return Color.Transparent; } }
 
         public char FillingChar { get { return '*'; } }
 
