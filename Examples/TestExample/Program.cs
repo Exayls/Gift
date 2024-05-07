e127d3878c1bde2134aa8bf3e47759d0a2a95329
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2024-05-07 11:24:41 +0200

    use hosted service

--- Examples/TestExample/Program.cs
+++ Examples/TestExample/Program.cs
@@ -1,14 +1,16 @@
 using Gift.ApplicationService.ServiceContracts;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Gift.Startup.Extensions;
 using Serilog;
+using System.Threading.Tasks;
+using System.Threading;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var services = new ServiceCollection();
-        services.AddGiftServices();
         Log.Logger = new LoggerConfiguration()
                          .WriteTo.File($"logs/HotReloadExample.log", rollingInterval: RollingInterval.Day)
                          .MinimumLevel.Debug()
@@ -16,9 +18,39 @@ internal class Program
         services.AddLogging(builder =>
                             { builder.AddSerilog(); });
         var serviceProvider = services.BuildServiceProvider();
-        var gift = serviceProvider.GetRequiredService<IGiftService>();
+        // var gift = serviceProvider.GetRequiredService<IGiftService>();
 
-        gift.InitializeHotReload("test.xml");
-        gift.Run();
+		IHostBuilder builder = Host.CreateDefaultBuilder();
+		builder.ConfigureServices(services =>
+		{
+			services.AddGiftServices();
+			services.AddHostedService<Worker>();
+		});
+
+		IHost host = builder.Build();
+		host.Run();
+    }
+}
+
+public class Worker : IHostedService
+{
+    private IGiftService _giftService;
+
+    public Worker(IGiftService giftService){
+		_giftService = giftService;
+	}
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+		_giftService.Initialize("test.xml");
+		// var task = Task.Run(()=> _giftService.Run());
+		var task = Task.CompletedTask;
+		_giftService.Run();
+		return task;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+		return Task.CompletedTask;
     }
 }
